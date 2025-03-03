e14d5010ea896ede0c7c51b6023f1c54193e3417
avery.white <averydouglascwhite@usf.edu>
2025-03-02 22:30:47 -0500

    Task 2 - Updated Metric Via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -11,3 +11,16 @@ function createMetricCard(id, title, initialValue) {
 
 createMetricCard("revenueCard", "Revenue", 0); //Invoking our createMetricCard() for the Revenue Card
 // Task 1 - Created Revenue Metric Card
+
+createMetricCard("profitCard", "Profit", 0); 
+createMetricCard("expensesCard", "Expenses", 0);
+
+const metricCardList = document.querySelectorAll(".metric-card"); 
+const metricCardArray = Array.from(metricCardList); 
+
+metricCardArray.forEach(card => { 
+    card.innerHTML += "<p><i> - Updated</i></p>"; 
+    card.style.backgroundColor = "greenyellow"; 
+});
+// Task 2 - Updated Metric Via Array Conversion
+
